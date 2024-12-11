871eff7834de7bf4c229932df427fd69d6ed6959
sadwiik06 <2300032291@kluniversity.in>
2024-12-11 10:50:00 +0530

    Updated .env and settings.py for database configuration

--- SMC/settings.py
+++ SMC/settings.py
@@ -77,14 +77,19 @@ WSGI_APPLICATION = 'SMC.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'SMC',         # The name of your database
-        'USER': 'postgres',        # Your PostgreSQL username
-        'PASSWORD': 'arunakatam85@SAI1',    # Your PostgreSQL password
-        'HOST': 'localhost',            # Set to empty string for localhost
-        'PORT': '5432',                 # Default PostgreSQL port
+        'NAME': os.getenv('DB_NAME', 'default_name'),
+        'USER': os.getenv('DB_USER', 'default_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'default_password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
